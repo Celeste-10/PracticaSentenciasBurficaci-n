fe0adeb66ecbb93bd287ea4279c4925660200209
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2026-03-16 15:59:15 -0600

    Ejercicio 3 completado

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un numero: ");
+        int dia= scanner.nextInt();
+
+        String nombreDia = switch (dia) {
+            case 1 -> "Lunes";
+            case 2 -> "Martes";
+            case 3 -> "Miércoles";
+            case 4 -> "Jueves";
+            case 5 -> "Viernes";
+            default -> "Fin de semana";
+        };
+        System.out.println(nombreDia);
+        scanner.close();
+    }
 }
