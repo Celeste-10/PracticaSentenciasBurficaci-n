900da4b4f75e1dc9ded4a4f0299da2070843a9d2
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2026-03-16 15:53:09 -0600

    Ejercicio 2 completado

--- src/Main/Ejercicio1.java
+++ src/Main/Ejercicio1.java
@@ -2,7 +2,7 @@ package Main;
 
 import java.util.Scanner;
 
-public class Main {
+public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
---
+++ src/Main/Ejercicio2.java
@@ -0,0 +1,68 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public class ValidadorFecha {
+
+        public static boolean esBisiesto(int anio) {
+            if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
+                return true;
+            }
+            return false;
+        }
+
+        public static boolean fechaValida(int dia, int mes, int anio) {
+
+            if (mes < 1 || mes > 12) {
+                return false;
+            }
+
+            int diasMes;
+
+            switch (mes) {
+                case 4: case 6: case 9: case 11:
+                    diasMes = 30;
+                    break;
+
+                case 2:
+                    if (esBisiesto(anio)) {
+                        diasMes = 29;
+                    } else {
+                        diasMes = 28;
+                    }
+                    break;
+
+                default:
+                    diasMes = 31;
+            }
+
+            if (dia < 1 || dia > diasMes) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Ingrese el día: ");
+            int dia = scanner.nextInt();
+
+            System.out.print("Ingrese el mes: ");
+            int mes = scanner.nextInt();
+
+            System.out.print("Ingrese el año: ");
+            int anio = scanner.nextInt();
+
+            if (fechaValida(dia, mes, anio)) {
+                System.out.println("La fecha es válida.");
+            } else {
+                System.out.println("La fecha NO es válida.");
+            }
+
+            scanner.close();
+        }
+    }
